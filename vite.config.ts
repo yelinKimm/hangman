d5b3fbddf442abf83f05c2ee3a8933110a712fc9
Iylin Kim <IYLIN@gimheungseon-ui-MacBookPro.local>
2024-03-06 18:01:19 +0900

    Modify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
+const env = loadEnv(
+  'all',
+  process.cwd()
+);
+
 export default defineConfig({
-    base: '/hangman/',
+    base: env.DEV ? env.DEV : '/',
     plugins: [react(), viteTsconfigPaths()],
     server: {    
       open: true,
