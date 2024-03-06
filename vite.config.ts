b14db2f88075c59a31fedadc11c052ac229e0514
Iylin Kim <IYLIN@gimheungseon-ui-MacBookPro.local>
2024-03-06 17:27:21 +0900

    Revert "Modify vite config"
    This reverts commit e75b500176c127a6a95f234bb50984b9d69e59e7.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
-const env = loadEnv(
-  'all',
-  process.cwd()
-);
- 
 export default defineConfig({
-    base: env.DEV,
+    base: '/hangman/',
     plugins: [react(), viteTsconfigPaths()],
     server: {    
       open: true,
